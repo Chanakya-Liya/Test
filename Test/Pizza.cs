6da5e1672fb2e65f827c5721b8aeabf41bfd2613
chanakya <chanakyal@zone24x7.com>
2023-06-01 00:49:19 +0530

    updated

--- Test/Pizza.cs
+++ Test/Pizza.cs
@@ -1,21 +1,78 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test
 {
     internal class Pizza
     {
         public string size;
-        public List<string> toppings;
+        public ArrayList toppings = new ArrayList();
         public int price = 0;
 
-        public Pizza(string size)
+        public Pizza()
         {
-            this.size = size;
-            toppings = new List<string>();
+            while (true)
+            {
+                Console.Write("What size Pizza would you like: ");
+                string size = Console.ReadLine();
+                if (string.Equals(size, "small", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.size = size;
+                    price += 10;
+                    break;
+                }
+                else if (string.Equals(size, "medium", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.size = size;
+                    price += 12;
+                    break;
+                }
+                else if (string.Equals(size, "large", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.size = size;
+                    price += 14;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Pizza size");
+                }
+            }
+           
+        }
+
+        public void pizzaToppings()
+        {
+            while(true)
+            {
+                Console.Write("What toppings would you like(Or enter done to end): ");
+                string topping = Console.ReadLine();
+                if (string.Equals(topping, "cheese", StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(topping, "ham", StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(topping, "pepperoni", StringComparison.OrdinalIgnoreCase))
+                {
+                    toppings.Add(topping);
+                    Console.WriteLine($"{topping} added");
+                    price += 2;
+
+                }else if(string.Equals(topping,"done", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid topping");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void viewToppings()
+        {
+            foreach(string topping in toppings)
+            {
+                Console.WriteLine($" -{topping}");
+            }
         }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -6,115 +6,54 @@ List<Pizza> pizzas = new List<Pizza>();
 Console.WriteLine("Welcome to Pizzaria");
 while (true)
 {
-    Console.Write("Enter Pizza size, Enter view to view all your pizzas or Enter quit to end: ");
-    string size = Console.ReadLine();
+    Console.Write("Enter Pizza to buy Pizza, view to view all your pizzas or quit to go to checkout: ");
+    string command = Console.ReadLine();
 
-    if (string.Equals(size, "quit", StringComparison.OrdinalIgnoreCase))
+    if (string.Equals(command, "pizza", StringComparison.OrdinalIgnoreCase))
     {
-        break;
-    }
-    if (string.Equals(size, "small", StringComparison.OrdinalIgnoreCase) ||
-        string.Equals(size, "medium", StringComparison.OrdinalIgnoreCase) ||
-        string.Equals(size, "large", StringComparison.OrdinalIgnoreCase))
-    {
-        Pizza pizza = new Pizza(size);
-
-        switch (size.ToLower())
-        {
-            case "small":
-                pizza.price += 10;
-                break;
-            case "medium":
-                pizza.price += 12;
-                break;
-            case "large":
-                pizza.price += 14;
-                break;
-        }
-
-        while (true)
-        {
-            Console.WriteLine();
-            Console.Write("Enter toppings or Enter done to end: ");
-
-            string topping = Console.ReadLine();
-
-            if (string.Equals(topping, "done", StringComparison.OrdinalIgnoreCase))
-            {
-                Console.WriteLine();
-                break;
-            }
-            if (string.Equals(topping, "Cheese", StringComparison.OrdinalIgnoreCase) ||
-                string.Equals(topping, "ham", StringComparison.OrdinalIgnoreCase) ||
-                string.Equals(topping, "pepperoni", StringComparison.OrdinalIgnoreCase))
-            {
-                pizza.toppings.Add(topping);
-                pizza.price += 2;
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("Invalid input please enter cheese, ham or pepperoni");
-            }
-
-        }
+        Pizza pizza = new Pizza();
+        pizza.pizzaToppings();
         pizzas.Add(pizza);
-
     }
-    else if (String.Equals(size, "view", StringComparison.OrdinalIgnoreCase))
+    else if (string.Equals(command, "view", StringComparison.OrdinalIgnoreCase))
     {
-        int j = 1;
-        foreach (Pizza pizza in pizzas)
-        {
-            Console.WriteLine(j + ".");
-            Console.WriteLine("Size: " + pizza.size);
-            Console.WriteLine("Toppings:");
+        viewPizza();
 
-            foreach (string topping in pizza.toppings)
-            {
-                Console.WriteLine("- " + topping);
-            }
-            Console.WriteLine("Price: $"+pizza.price);
+    }else if(string.Equals(command, "quit", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.Write("Enter coupon code: ");
+        Console.ReadLine();
+        viewPizza();
+        Console.WriteLine($"Total Price: ${totalPrice()}");
+        break;
 
-            Console.WriteLine();
-            j++;
-        }
     }
     else
     {
-        Console.WriteLine();
-        Console.WriteLine("Invalid input");
-        Console.WriteLine("Please enter small medium or large");
+        Console.WriteLine("Invalid Input");
     }
 }
 
-Console.WriteLine();
-Console.Write("Enter coupon code: ");
-string coupon = Console.ReadLine();
-Console.WriteLine();
-
-int i = 1;
-foreach (Pizza pizza in pizzas)
+void viewPizza()
 {
-    Console.WriteLine(i + ".");
-    Console.WriteLine("Size: " + pizza.size);
-    Console.WriteLine("Toppings:");
-
-    foreach (string topping in pizza.toppings)
+    int i = 1;
+    foreach (Pizza pizza in pizzas)
     {
-        Console.WriteLine("- " + topping);
+        Console.WriteLine($"{i}.Size: {pizza.size}");
+        pizza.viewToppings();
+        Console.WriteLine($"Price: ${pizza.price}");
+        Console.WriteLine();
+        i++;
     }
-    Console.WriteLine("Price: $" + pizza.price);
-
-    Console.WriteLine();
-    i++;
 }
 
-Console.WriteLine();
-int TotalPrice = 0;
-Console.Write("Total Price: $");
-foreach (Pizza pizza in pizzas)
+int totalPrice()
 {
-    TotalPrice += pizza.price;
+    int price = 0;
+    foreach(Pizza pizza in pizzas)
+    {
+        price += pizza.price;
+    }
+    return price;
 }
-Console.WriteLine(TotalPrice);
+
